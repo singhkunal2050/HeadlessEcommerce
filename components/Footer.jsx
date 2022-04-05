6c8f6b679a7245ba5400a4814851bce0fc3306a5
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-05 18:55:12 +0530

    updates

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer>
-        <div className="container">
+    <footer className='bg-rose-700' >
+        <div className="container max-w-5xl mx-auto text-center text-white py-10  ">
             &copy; 2021 - Headless Ecommerce
         </div>
     </footer>
