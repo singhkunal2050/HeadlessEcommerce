5c94a0d82d6bb61d49f33f347cc9aa2994c8d4f5
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-06 13:41:34 +0530

    updated Products Content

--- context/productsContext.js
+++ context/productsContext.js
@@ -1,3 +1,15 @@
-import { createContext } from "react";
+import { createContext , useState } from "react";
 
 export const ProductContext = createContext({})
+
+export const ProductProvider = (props) =>{
+    let [productsGlobal, setProductsGlobal] = useState();
+
+
+    const productStore = {productsGlobal, setProductsGlobal}
+    return(
+        <ProductContext.Provider value={productStore}>
+            {props.children}
+        </ProductContext.Provider>
+    )
+}
--- pages/_app.js
+++ pages/_app.js
@@ -2,22 +2,21 @@ import Cart from "../components/Cart";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import "../styles/globals.css";
-import { CartContext, CartProvider } from "../context/cartContext";
-import { ProductContext } from "../context/productsContext";
+import { CartProvider } from "../context/cartContext";
+import {  ProductProvider } from "../context/productsContext";
 import { useState } from "react";
 
 function MyApp({ Component, pageProps }) {
-  let [productsGlobal, setProductsGlobal] = useState();
   return (
     <>
      
       <CartProvider>
-        <ProductContext.Provider value={{ productsGlobal, setProductsGlobal }}>
+        <ProductProvider>
           <Navbar />
           <Cart />
           <Component {...pageProps} />
           <Footer />
-        </ProductContext.Provider>
+        </ProductProvider>
       </CartProvider>
     </>
   );
