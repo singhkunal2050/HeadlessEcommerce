f348f8efa103909b73efb9bcbfba662c1ae42afe
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-08 16:37:33 +0530

    updated

---
+++ components/FAQ.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import Container from './Container'
+
+function FAQ() {
+  return (
+    <Container>
+    <section className="py-8">
+      <h1 className="text-3xl font-bold text-center py-4">FAQ'S </h1>
+      <section className="grid grid-cols-1 md:grid-cols-2 gap-2 py-4  ">
+        <div className="p-6 shadow-lg">
+          <h4 className="text-xl text-darknight font-bold mb-2">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolores, libero?{" "}
+          </h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
+            harum cumque ab impedit consequuntur assumenda hic nesciunt
+            cupiditate dignissimos quibusdam dicta ratione?
+          </p>
+          <button className="bg-darknight text-white px-3 py-2 mt-4">
+            Know More
+          </button>
+        </div>
+        <div className="p-6 shadow-lg">
+          <h4 className="text-xl text-darknight font-bold mb-2">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolores, libero?{" "}
+          </h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
+            harum cumque ab impedit consequuntur assumenda hic nesciunt
+            cupiditate dignissimos quibusdam dicta ratione?
+          </p>
+          <button className="bg-darknight text-white px-3 py-2 mt-4">
+            Know More
+          </button>
+        </div>
+        <div className="p-6 shadow-lg">
+          <h4 className="text-xl text-darknight font-bold mb-2">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolores, libero?{" "}
+          </h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
+            harum cumque ab impedit consequuntur assumenda hic nesciunt
+            cupiditate dignissimos quibusdam dicta ratione?
+          </p>
+          <button className="bg-darknight text-white px-3 py-2 mt-4">
+            Know More
+          </button>
+        </div>
+        <div className="p-6 shadow-lg">
+          <h4 className="text-xl text-darknight font-bold mb-2">
+            {" "}
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Dolores, libero?{" "}
+          </h4>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
+            harum cumque ab impedit consequuntur assumenda hic nesciunt
+            cupiditate dignissimos quibusdam dicta ratione?
+          </p>
+          <button className="bg-darknight text-white px-3 py-2 mt-4">
+            Know More
+          </button>
+        </div>
+      </section>
+    </section>
+  </Container>
+  )
+}
+
+export default FAQ
--- pages/index.jsx
+++ pages/index.jsx
@@ -8,6 +8,7 @@ import {
   AiFillSafetyCertificate,
   AiOutlineUser,
 } from "react-icons/ai";
+import FAQ from "../components/FAQ";
 
 export default function Home() {
   return (
@@ -55,77 +56,8 @@ export default function Home() {
         </h4>
       </section>
 
-      <Container>
-        <section className="py-8">
-          <h1 className="text-3xl font-bold text-center py-4">FAQ'S </h1>
-          <section className="grid grid-cols-1 md:grid-cols-2 gap-2 py-4  ">
-            <div className="p-6 shadow-lg">
-              <h4 className="text-xl text-darknight font-bold mb-2">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores, libero?{" "}
-              </h4>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
-                harum cumque ab impedit consequuntur assumenda hic nesciunt
-                cupiditate dignissimos quibusdam dicta ratione?
-              </p>
-              <button className="bg-darknight text-white px-3 py-2 mt-4">
-                Know More
-              </button>
-            </div>
-            <div className="p-6 shadow-lg">
-              <h4 className="text-xl text-darknight font-bold mb-2">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores, libero?{" "}
-              </h4>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
-                harum cumque ab impedit consequuntur assumenda hic nesciunt
-                cupiditate dignissimos quibusdam dicta ratione?
-              </p>
-              <button className="bg-darknight text-white px-3 py-2 mt-4">
-                Know More
-              </button>
-            </div>
-            <div className="p-6 shadow-lg">
-              <h4 className="text-xl text-darknight font-bold mb-2">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores, libero?{" "}
-              </h4>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
-                harum cumque ab impedit consequuntur assumenda hic nesciunt
-                cupiditate dignissimos quibusdam dicta ratione?
-              </p>
-              <button className="bg-darknight text-white px-3 py-2 mt-4">
-                Know More
-              </button>
-            </div>
-            <div className="p-6 shadow-lg">
-              <h4 className="text-xl text-darknight font-bold mb-2">
-                {" "}
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                Dolores, libero?{" "}
-              </h4>
-              <p>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Accusamus, ex sequi? Nemo quo perspiciatis, nam voluptas facere
-                harum cumque ab impedit consequuntur assumenda hic nesciunt
-                cupiditate dignissimos quibusdam dicta ratione?
-              </p>
-              <button className="bg-darknight text-white px-3 py-2 mt-4">
-                Know More
-              </button>
-            </div>
-          </section>
-        </section>
-      </Container>
+      <FAQ/>
+    
     </main>
   );
 }
