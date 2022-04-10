eba2c99394ec415bb8aadf03afce478e848adb8c
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-10 18:12:17 +0530

    updated mobile container

--- components/Container.jsx
+++ components/Container.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Container(props) {
   return (
-    <div className="container mx-auto max-w-5xl">
+    <div className="container mx-auto max-w-5xl px-2">
       {props.children}
     </div>
   )
