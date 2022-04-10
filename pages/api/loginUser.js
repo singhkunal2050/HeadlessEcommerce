2a6416c81b6a0831998a8620b7b1b860c26c6dac
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-10 19:49:28 +0530

    created login toute

---
+++ pages/api/loginUser.js
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import connect from "../../config/dbconfig";
+import User from "../../models/User";
+
+export default async function handler(req, res) {
+  const { method } = req;
+  try {
+    await connect();
+    switch (method) {
+      case "POST": {
+        const user = await User.findOne({email:req.body.email ,password:req.body.password});
+        if(user){
+          res.json({ success:true , "id":user._id});
+        }else{
+          res.json({ success:false });
+        }
+      }
+      default : {
+        res.json({ success:false });
+      }
+    }
+  } catch (err) {
+    res.json({ success:false , err});
+    console.log(err);
+  }
+}
