2ebb05f9bbca81bd0c1e2b3ff215b94aefbf0814
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-11 19:03:41 +0530

    bug fixes

--- components/Signup.jsx
+++ components/Signup.jsx
@@ -157,7 +157,7 @@ function Signup({ updateComponent }) {
             <div className={`spinner ${loading == true ? "" : "hidden"}  `}>
               <svg
                 role="status"
-                class="mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white"
+                className="mr-2 w-4 h-4 text-gray-200 animate-spin dark:text-gray-600 fill-white"
                 viewBox="0 0 100 101"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
