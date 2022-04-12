bb9a52bd9066c16fd7c40a0f6ef8e4148d4d3c89
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-12 16:32:28 +0530

    Updated footer 😁

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <footer className='bg-darknight  font-montserrat ' >
-        <div className="container max-w-5xl mx-auto text-center text-primaryaccent py-10  ">
-            &copy; 2021 - Headless Ecommerce
-        </div>
+    <footer className="bg-darknight  font-montserrat ">
+      <div className="container max-w-5xl mx-auto text-center text-primaryaccent py-10  ">
+        <p>&copy; 2021 - Headless Ecommerce</p>
+        <p className="pt-2">
+          Made with  ♥  by 
+            <a className="px-1 font-bold" target="_blank"  href="https://www.singhkunal2050.dev"> @singhkunal2050 </a>
+        </p>
+      </div>
     </footer>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
