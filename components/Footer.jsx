351d9f2df4821f8c3e063df197eea70a61426293
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-12 19:05:51 +0530

    added stack images in footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Image from "next/image";
 
 function Footer() {
   return (
@@ -6,11 +7,26 @@ function Footer() {
       <div className="container max-w-5xl mx-auto text-center text-primaryaccent py-10  ">
         <p>&copy; 2021 - Headless Ecommerce</p>
         <p className="pt-2">
-          Made with  ♥  by 
-            <a className="px-1 font-bold" target="_blank" rel="noreferrer" href="https://www.singhkunal2050.dev"> @singhkunal2050 </a>
+          Made with ♥ by
+          <a
+            className="px-1 font-bold"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.singhkunal2050.dev"
+          >
+            {" "}
+            @singhkunal2050{" "}
+          </a>
         </p>
         <h2 className="pt-2 pb-2 text-lg font-bold">Stack</h2>
-        <p > Nextjs | Mongodb | TailwindCSS | JWT | Shopify Storefront API</p>
+        <p> Nextjs | TailwindCSS | JWT | Mongodb | Shopify Storefront API</p>
+        <div className="flex gap-4 justify-center pt-4">
+          <Image src="/next-js.svg" width={40} height={40} />
+          <Image src="/tailwind.png" width={40} height={40} />
+          <Image src="/jwt.png" width={40} height={40} />
+          <Image src="/mongodb.svg" width={40} height={40} />
+          <Image src="/shopify.png" width={40} height={40} />
+        </div>
       </div>
     </footer>
   );
