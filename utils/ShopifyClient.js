b56c04ec85cb83941a2b71a5e6a24cddde651ec4
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-14 18:34:48 +0530

    updated API Tokens 🎭

--- utils/ShopifyClient.js
+++ utils/ShopifyClient.js
@@ -1,5 +1,5 @@
 import Client from "shopify-buy";
 export  const client = Client.buildClient({
-  domain: process.env.SHOPIFY_API_ENDPOINT ?? "diljit-developers.myshopify.com",
-  storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_API_TOKEN ?? "107be587cc38f0d2f2763e73835a3866"
+  domain: process.env.NEXT_PUBLIC_SHOPIFY_API_ENDPOINT ,
+  storefrontAccessToken: process.env.NEXT_PUBLIC_SHOPIFY_STOREFRONT_API_TOKEN
  });
