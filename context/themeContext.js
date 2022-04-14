1ca660e5d71ea12f017d91feb3fb6ea1f86b6be6
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-14 15:10:22 +0530

    fixed bugs on light dark 😀

--- context/themeContext.js
+++ context/themeContext.js
@@ -1,29 +1,38 @@
-import { createContext , useState } from "react"
+import { createContext, useState } from "react";
+import { useEffect } from "react";
 
-export const ThemeContext = createContext('default')
+export const ThemeContext = createContext("default");
 
-export const ThemeProvider =  (props) =>{
-  let [theme, setTheme] = useState('default')
+export const ThemeProvider = (props) => {
+  let [theme, setTheme] = useState("default");
 
-  if(localStorage.getItem('theme') === 'dark' || window.matchMedia('(prefers-color-scheme: dark)').matches){
-    document.documentElement.classList.add('dark');
-  }
-
-  const toggleTheme = () =>{
-    if (theme!='dark') {
-        localStorage.theme='dark';
-        setTheme('dark')
-        document.documentElement.classList.add('dark');
-    } else {
-        localStorage.theme='light';
-        setTheme('light')
-        document.documentElement.classList.remove('dark')
+  useEffect(() => {
+    if (
+      localStorage.getItem("theme") === "dark" ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
     }
-  }
+  });
 
-  let themeStore = { theme , toggleTheme }
+  const toggleTheme = () => {
+    if (theme != "dark") {
+      localStorage.theme = "dark";
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      localStorage.theme = "light";
+      setTheme("light");
+      document.documentElement.classList.remove("dark");
+    }
+  };
 
-  return (<ThemeContext.Provider value={themeStore}>
-    {props.children}
-  </ThemeContext.Provider>)
-}
+  let themeStore = { theme, toggleTheme };
+
+  return (
+    <ThemeContext.Provider value={themeStore}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
+};
