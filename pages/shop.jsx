4d99fd86f60c872c70eb25a13b138adc1bd0293b
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-15 13:12:49 +0530

    added Meta to Shop

--- pages/shop.jsx
+++ pages/shop.jsx
@@ -32,48 +32,54 @@ function Shop({ products, nextPage }) {
   // console.log(nextPage);
 
   return (
-    <main className="shop font-montserrat  ">
-      <Container>
-        <div className=" p-4 md:px-10 flex gap-4 items-center sticky top-[60px] left-0 right-0 z-[1]">
-          <input
-            value={search}
-            onChange={(e) => setsearch(e.target.value)}
-            className="px-3 py-2 w-full md:w-[80%] rounded-sm border-2 dark:bg-gray-600 dark:border-gray-600 mx-auto "
-            type="text"
-            placeholder="Search..."
-          />
-        </div>
-        <div className="products grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-2 md:px-20  ">
-          {!search ? (
-            products.map((product) => {
-              return <ProductSingle key={product.id} product={product} />;
-            })
-          ) : (
-            <>
-              {productsGlobal
-                .filter((product) =>
-                  product.title.toLowerCase().includes(search.toLowerCase())
-                )
-                .map((product) => {
-                  return <ProductSingle key={product.id} product={product} />;
-                })}
-            </>
-          )}
-        </div>
-        <div className="flex justify-center py-4">
-          <button className="bg-darknight py-2 px-3 text-center mx-auto">
-            Load More
-          </button>
-        </div>
-      </Container>
-    </main>
+    <>
+      <Head>
+        <title>HeadLess Commerce | Shop</title>
+      </Head>
+
+      <main className="shop font-montserrat  ">
+        <Container>
+          <div className=" p-4 md:px-10 flex gap-4 items-center sticky top-[60px] left-0 right-0 z-[1]">
+            <input
+              value={search}
+              onChange={(e) => setsearch(e.target.value)}
+              className="px-3 py-2 w-full md:w-[80%] rounded-sm border-2 dark:bg-gray-600 dark:border-gray-600 mx-auto "
+              type="text"
+              placeholder="Search..."
+            />
+          </div>
+          <div className="products grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-2 md:px-20  ">
+            {!search ? (
+              products.map((product) => {
+                return <ProductSingle key={product.id} product={product} />;
+              })
+            ) : (
+              <>
+                {productsGlobal
+                  .filter((product) =>
+                    product.title.toLowerCase().includes(search.toLowerCase())
+                  )
+                  .map((product) => {
+                    return <ProductSingle key={product.id} product={product} />;
+                  })}
+              </>
+            )}
+          </div>
+          <div className="flex justify-center py-4">
+            <button className="bg-darknight py-2 px-3 text-center mx-auto">
+              Load More
+            </button>
+          </div>
+        </Container>
+      </main>
+    </>
   );
 }
 
 export default Shop;
 
 export async function getStaticProps(context) {
-  let products = await client.product.fetchQuery({first:80});
+  let products = await client.product.fetchQuery({ first: 80 });
   // let products = await client.product.fetchAll(10);
   let nextPage = await client.fetchNextPage(products);
   let infos = await client.shop.fetchInfo(); // Fetch shop Info if you think about SEO and title and ... to your page
