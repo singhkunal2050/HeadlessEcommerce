53614b092e356e67963dd07d4f23e308dd10df54
singhkunal2050 <singhkunal2050@gmail.com>
2022-04-18 09:41:08 +0530

    setup for future pagination 🌏

--- pages/shop.jsx
+++ pages/shop.jsx
@@ -9,9 +9,9 @@ function Shop({ products, nextPage }) {
   const { productsGlobal, setProductsGlobal } = useContext(ProductContext);
   const [search, setsearch] = useState();
 
-  console.log(products);
   useEffect(() => {
     setProductsGlobal(products);
+    console.log(products)
   }, []);
 
   // For Future pagination requirements
@@ -49,30 +49,29 @@ function Shop({ products, nextPage }) {
               placeholder="Search..."
             />
           </div>
-          <div className="products grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-2 md:px-20  ">
-            {!search ? (
+          <div className="products grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 py-8 px-2 md:px-20 ">
+            {!search && products ? (
               products.map((product) => {
                 return <ProductSingle key={product.id} product={product} />;
               })
             ) : (
               <>
-                {productsGlobal
+                {productsGlobal ? productsGlobal
                   .filter((product) =>
                     product.title.toLowerCase().includes(search.toLowerCase())
                   )
                   .map((product) => {
                     return <ProductSingle key={product.id} product={product} />;
-                  })}
+                  }) : <p className="text-center mb-6">No Products Found</p>  }
               </>
             )}
           </div>
-          
+
           {/* <div className="flex justify-center py-4">
             <button className="bg-darknight py-2 px-3 text-center mx-auto">
               Load More
             </button>
           </div> */}
-
         </Container>
       </main>
     </>
@@ -81,24 +80,34 @@ function Shop({ products, nextPage }) {
 
 export default Shop;
 
-export async function getStaticProps(context) {
-  let products = await client.product.fetchQuery({ first: 80 });
-  // let products = await client.product.fetchAll(10);
-  let nextPage = await client.fetchNextPage(products);
-  let infos = await client.shop.fetchInfo(); // Fetch shop Info if you think about SEO and title and ... to your page
-  let policies = await client.shop.fetchPolicies(); // fetch shop policy if you have any
+export async function getServerSideProps({query}) {
+  // console.log(query.page)
+  try {
+    let products = await client.product.fetchQuery({ first: 80 });
+    // let products = await client.product.fetchAll(10);
+    let nextPage = await client.fetchNextPage(products);
+    let infos = await client.shop.fetchInfo(); // Fetch shop Info if you think about SEO and title and ... to your page
+    let policies = await client.shop.fetchPolicies(); // fetch shop policy if you have any
 
-  nextPage = JSON.parse(JSON.stringify(nextPage));
-  products = JSON.parse(JSON.stringify(products));
-  infos = JSON.parse(JSON.stringify(infos));
-  policies = JSON.parse(JSON.stringify(policies));
+    nextPage = JSON.parse(JSON.stringify(nextPage));
+    products = JSON.parse(JSON.stringify(products));
+    infos = JSON.parse(JSON.stringify(infos));
+    policies = JSON.parse(JSON.stringify(policies));
 
-  return {
-    props: {
-      products,
-      nextPage,
-      infos,
-      policies,
-    }, // will be passed to the page component as props
-  };
+    return {
+      props: {
+        products,
+        nextPage,
+        infos,
+        policies,
+      }, // will be passed to the page component as props
+    };
+  } catch (err) {
+    let e = JSON.parse(JSON.stringify(err))
+    return {
+      props: {
+        e,
+      }, // will be passed to the page component as props
+    };
+  }
 }
